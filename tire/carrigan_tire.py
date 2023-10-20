40a59a69f2742595194273b64a3699b9453a5ae5
xshadowlegendx <lyheng0@gmail.com>
2023-10-20 23:44:25 +0700

    change to float instead of int

--- tire/carrigan_tire.py
+++ tire/carrigan_tire.py
@@ -2,7 +2,7 @@
 from tire.tire import Tire
 
 class CarriganTire(Tire):
-    def __init__(self, wearings: list[int]):
+    def __init__(self, wearings: list[float]):
         self.wearings = wearings
         self.__is_need_service = len([w for w in self.wearings if w >= 0.9]) > 1
 
--- tire/octoprime_tire.py
+++ tire/octoprime_tire.py
@@ -2,7 +2,7 @@
 from tire.tire import Tire
 
 class OctoprimeTire(Tire):
-    def __init__(self, wearings: list[int]):
+    def __init__(self, wearings: list[float]):
         self.wearings = wearings
         self.__is_need_service = sum(wearings) >= 3
 
